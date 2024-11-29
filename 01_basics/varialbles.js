02c950a8750d3d045c43d621c447ce73bdfd5ee2
parthdevwork <138773451+parthdevwork@users.noreply.github.com>
2024-11-29 05:57:56 +0000

    varibles file inluceded

---
+++ 01_basics/varialbles.js
@@ -0,0 +1,11 @@
+const accounId = 1445533    
+
+let accounEmail = "vaishnavi@gmail.com"
+ var accountPassword = "123456"
+ aacountCity = "surat"
+
+//  accounId ="geetu@gmail.com" can't chnage in this 
+
+ console.log(accounEmail);
+ 
+ console.table([accounEmail,accounId,accountPassword,aacountCity])
