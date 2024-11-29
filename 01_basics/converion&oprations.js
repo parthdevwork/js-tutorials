d447babfe839b36c7cb592ae50a4e3a75909ce8c
parthdevwork <138773451+parthdevwork@users.noreply.github.com>
2024-11-29 07:07:35 +0000

    conert the datatypes in another datatypes

---
+++ 01_basics/converion&oprations.js
@@ -0,0 +1,41 @@
+//thi is the examples of conert string to number
+
+let string = "vaishnavi"
+
+let stringToNum = Number(string)
+console.log(typeof stringToNum)
+
+// this is the example of number to string
+
+let number = 123
+
+let numToString = String(number)
+
+console.log(typeof numToString)
+
+
+// now mix the number and string conert and which type provide see...
+
+let boTh = "123asb"
+let stringNum = Number(boTh)
+
+console.log(typeof stringNum);
+console.log(stringNum)
+
+// now see the undefind convert
+
+let undefind = undefined
+let type = Number(undefind)
+
+console.log(typeof type);
+console.log(type)
+
+
+// now see Null what the return output
+
+
+let nullString = null
+let stringConvrt = Number(null)
+
+console.log(typeof stringConvrt);
+console.log(stringConvrt)
