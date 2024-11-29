7bf644434348d06c5d2a49d03ba4a39c9b4bdf2f
parthdevwork <138773451+parthdevwork@users.noreply.github.com>
2024-11-29 06:28:14 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+let name = "vaishnavi"
+console.log(typeof name)
+
+let number = 123
+console.log(typeof number)
+
+let undefined;
+console.log(typeof undefined)
+
+
+let usingNull= null
+console.log(typeof usingNull) // null is a object got it remeber you
+
+// datatypes in js
+
+//string
+//number
+//bigint
+//null
+//undefined
+//object
+
+// this are the datatypes in js 
