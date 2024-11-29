fcfa057229cd5c2bfe012995c253c40cafc80e18
parthdevwork <138773451+parthdevwork@users.noreply.github.com>
2024-11-29 09:33:59 +0000

    string , comparison and summery

---
+++ 01_basics/datatypes_summuery.js
@@ -0,0 +1,7 @@
+// premitive (call by value)
+// 7 types of premitive datatypes string  ,number , boolean , null , undefined , symbol , bigint
+
+
+// Non premitive (call by reference)
+
+// Array , object , functions
---
+++ 01_basics/string.js
@@ -0,0 +1,4 @@
+const name = "vaishnavi";
+const repoCount = 4
+
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`)
