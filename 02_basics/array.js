76d3e8dfb1b57a1e4e1330fce8669ded98afe94a
parthdevwork <138773451+parthdevwork@users.noreply.github.com>
2024-12-02 04:18:18 +0000

    learning about array

---
+++ 02_basics/array.js
@@ -0,0 +1,32 @@
+// arrays
+
+
+const myArry = [1,2,3,4,5,6]
+console.log(myArry);
+console.log(myArry[1]);
+// this is a very simple 
+
+
+const myNames = new Array('vaish','navi' ,'anil' ,'mahajan')
+console.log(myNames);
+console.log(myNames[3])
+
+
+// now see Array methods
+
+//push :- this push method is add the value in array you want
+//pop  :- this method is delete the value you want 
+// unshift :- this method is add the value in first of index
+//shift :- thiss method is delete the value from starting
+
+//  now i am using this all methods in below the code
+
+
+
+myArry.push(11)
+myArry.pop(11)
+myArry.unshift(12)
+myArry.shift()
+myArry.shift()
+
+console.log(myArry);
